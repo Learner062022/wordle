40dfb07e33da835a5386a19f49a572b0582d5a21
Learner062022 <110619033+Learner062022@users.noreply.github.com>
2022-12-10 09:38:42 +0800

    Commiting doctest

---
+++ doctest.py
@@ -0,0 +1,107 @@
+def is_correct(score):
+    """Checks if the score is entirely correct and returns True if it is
+    Examples:
+    >>> is_correct((1,1,1,1,1))
+    False
+    >>> is_correct((2,2,2,2,1))
+    False
+    >>> is_correct((0,0,0,0,0))
+    False
+    >>> is_correct((2,2,2,2,2))
+    True"""
+    return False
+
+def get_valid_words(file_path=ALL_WORDS):
+    """returns a list containing all valid words.
+    Note to test that the file is read correctly, use:
+    >>> get_valid_words()[0]
+    'aahed'
+    >>> get_valid_words()[-1]
+    'zymic'
+    >>> get_valid_words()[10:15]
+    ['abamp', 'aband', 'abase', 'abash', 'abask']
+
+    """
+    # read words from files and return a list containing all words that can be entered as guesses
+
+    return ['wibble', 'wobble', 'wubble', 'wabble']
+
+def get_target_word(file_path=TARGET_WORDS, seed=None):
+    """Picks a random word from a file of words
+
+    Args:
+        file_path (str): the path to the file containing the words
+
+    Returns:
+        str: a random word from the file
+
+    How do you test that a random word chooser is choosing the correct words??
+    Discuss in class!
+    >>> get_target_word()
+    'aback'
+    >>> get_target_word()
+    'zonal'
+
+    """
+    # read words from a file and return a random word (word of the day)
+    return 'woble'
+
+def ask_for_guess(valid_words):
+    """Requests a guess from the user directly from stdout/in
+    Returns:
+        str: the guess chosen by the user. Ensures guess is a valid word of correct length in lowercase
+    """
+    return 'wibble'
+
+def score_guess(guess, target_word):
+    """given two strings of equal length, returns a tuple of ints representing the score of the guess
+    against the target word (MISS, MISPLACED, or EXACT)
+    Here are some example (will run as doctest):
+
+    >>> score_guess('hello', 'hello')
+    (2, 2, 2, 2, 2)
+    >>> score_guess('drain', 'float')
+    (0, 0, 1, 0, 0)
+    >>> score_guess('hello', 'spams')
+    (0, 0, 0, 0, 0)
+
+    Try and pass the first few tests in the doctest before passing these tests.
+    >>> score_guess('gauge', 'range')
+    (0, 2, 0, 2, 2)
+    >>> score_guess('melee', 'erect')
+    (0, 1, 0, 1, 0)
+    >>> score_guess('array', 'spray')
+    (0, 0, 2, 2, 2)
+    >>> score_guess('train', 'tenor')
+    (2, 1, 0, 0, 1)
+        """
+    # You must use this convention as test automation will be validating your scorer
+    return 0, 0, 0, 0, 0
+
+def format_score(guess, score):
+    """Formats a guess with a given score as output to the terminal.
+    The following is an example output (you can change it to meet your own creative ideas, 
+    but be sure to update these examples)
+    >>> print(format_score('hello', (0,0,0,0,0)))
+    H E L L O
+    _ _ _ _ _
+    >>> print(format_score('hello', (0,0,0,1,1)))
+    H E L L O
+    _ _ _ ? ?
+    >>> print(format_score('hello', (1,0,0,2,1)))
+    H E L L O
+    ? _ _ + ?
+    >>> print(format_score('hello', (2,2,2,2,2)))
+    H E L L O
+    + + + + +"""
+    pass
+
+def main(test=False):
+    if test:
+        import doctest
+        return doctest.testmod()
+    play()
+
+
+if __name__ == '__main__':
+    print(main(test=True))
