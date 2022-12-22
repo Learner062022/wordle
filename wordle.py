acaebf52371dd691ce5bc838662f26929f7f5c34
L <110619033+Learner062022@users.noreply.github.com>
2022-12-22 23:02:04 +0800

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -2,7 +2,7 @@ def get_random_word():
     """Chooses a word at random"""
     lst_target_words = []
     import random
-    open_target_words = open('target_words.txt')
+    open_target_words = open("target_words.txt")
     for word in open_target_words:
         lst_target_words.append(word.strip())
     open_target_words.close()
@@ -17,7 +17,7 @@ print(answer)
 def get_dict_words():
     """lists dictionary words"""
     lst_dict_words = []
-    open_all_words_file = open('all_words.txt')
+    open_all_words_file = open("all_words.txt")
     for line in open_all_words_file:
         lst_dict_words.append(line.strip())
     open_all_words_file.close()
@@ -41,17 +41,16 @@ def validate_guess(answer):
             elif prompt_user not in dict_words:
                 prompt_user = input("The word doesn't exist - Enter another word ")
 
-                
 def validate_letters(answer):
-    """Joins the letters that are not in the guess and the positions of the guesses letters after their 
-    positions are checked 
+    """Joins the letters that are not in the guess and the positions of the guesses letters after their
+    positions are checked
     """
     position_letters = [0, 0, 0, 0, 0]
     letters_checked = [0, 0, 0, 0, 0]
     incorrect_letters = []
     validated_guess = validate_guess(answer)
     rev_validation = validated_guess[::-1]
-    counter = 0
+    counter = 0 
     while counter != 5:
         for letter in validated_guess:
             if letter not in answer:
@@ -71,7 +70,16 @@ def validate_letters(answer):
                 if validated_guess.count(letter) != answer.count(letter):
                     position_letters[counter] = "0"
                     letters_checked[counter] = "1"
+                    if letter in answer:
+                        position_letters[validated_guess.index(letter)] = "2"
+                        letters_checked[validated_guess.index(letter)] = "1"
+                        position_letters[rev_validation.find(letter)] = "0"
+                        letters_checked[rev_validation.find(letter)] = "1"
             counter += 1
+        if counter == 5:
+            " ".join(position_letters)
+            " ".join(incorrect_letters)
+            return position_letters, incorrect_letters
              
         
 def colour_letters(answer):
