8224d2ea86c46562ece7b51dbeac059fecf027fa
L <110619033+Learner062022@users.noreply.github.com>
2023-01-07 01:51:19 +0800

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -22,71 +22,80 @@ def get_dict_words():
     return dict_words
 
 
-def validate_guesses():
+def validate_guess():
     """Verifies guesses' length and legibility"""
     dict_words = get_dict_words()
-    guesses = []
-    while len(guesses) != 6:
-        prompt_user = input("Enter a 5 lettered word here ")
-        size_guess = len(prompt_user)
-        if size_guess != 5:
-            if size_guess > 5:
-                prompt_user = input("The word has more than 5 characters - Enter another word here ")
-            if size_guess < 5:
-                prompt_user = input("The word has less than 5 characters - Enter another word here ")
-        if size_guess == 5:
-            if prompt_user in dict_words:
-                guesses.append(prompt_user)
-            if prompt_user not in dict_words:
-                prompt_user = input("The word doesn't exist - Enter another word here ")
-    else:
-        return guesses
-
+    prompt_user = input("Enter a 5 lettered word here ")
+    size_guess = len(prompt_user)
+    if size_guess != 5:
+        if size_guess > 5:
+            prompt_user = input("The word has more than 5 characters - Enter another word here ")
+        if size_guess < 5:
+            prompt_user = input("The word has less than 5 characters - Enter another word here ")
+    if size_guess == 5:
+        if prompt_user in dict_words:
+            return prompt_user
+        if prompt_user not in dict_words:
+            prompt_user = input("The word doesn't exist - Enter another word here ")
+    
                 
 def validate_guesses_letters_positions(answer):
-    guesses_positions_letters = []
-    validated_guesses = validate_guesses()
-    for index_guess in range(len(validated_guesses)):
-        lst_guess = list(validated_guesses[index_guess])
-        for index_letter in range(len(lst_guess)):
-            if lst_guess[index_letter] not in answer:
-                lst_guess[index_letter] = "0"
-            if lst_guess[index_letter] == answer[index_letter]:
-                lst_guess[index_letter] = "1"
-            # if lst_guess[index_letter] in answer:
-            #     if lst_guess[index_letter] != answer[index_letter]:
-            #          if lst_guess.count(lst_guess[index_letter]) == answer.count(answer[index_letter]):
-            #          Resolve later
-            #          if lst_guess.count(lst_guess[index_letter]) != answer.count(answer[index_letter]):
-            #          Resolve later
-            #          Need to append incorrect letters to a list
-            if index_letter == 4:
-                lst_guess = "".join(lst_guess)
-                guesses_positions_letters.append(lst_guess)
-                if len(guesses_positions_letters) == 6:
-                    return guesses_positions_letters, validated_guesses
-                
+    validated_guess = validate_guess()
+    incorrect_letters = []
+    lst_guess = list(validated_guess)
+    for index_letter in range(len(validated_guess)):
+        if validated_guess[index_letter] not in answer:
+            lst_guess[index_letter] = "0"
+            if validated_guess[index_letter] not in incorrect_letters:
+                incorrect_letters.append(validated_guess[index_letter])
+        if validated_guess[index_letter] == answer[index_letter]:
+            lst_guess[index_letter] = "1"
+        if validated_guess[index_letter] in answer:
+            if validated_guess[index_letter] != answer[index_letter]:
+                 if validated_guess.count(validated_guess[index_letter]) == answer.count(answer[index_letter]):
+                     lst_guess[index_letter] = "2"
+        #          if lst_guess.count(lst_guess[index_letter]) != answer.count(answer[index_letter]):
+        #          Resolve later
+        if index_letter == 4:
+            return lst_guess, validated_guess, incorrect_letters
+            
       
 def colour_guesses_letters(answer):
-    positions_letters, predictions = validate_guesses_letters_positions(answer)
-    coloured_guesses = []
+    positions_letters, prediction, incorrect_letters = validate_guesses_letters_positions(answer)
+    lst_prediction = list(prediction)
     from termcolor import colored
     for index_positions_letters in range(len(positions_letters)):
-        prediction = predictions[index_positions_letters]
-        positions = positions_letters[index_positions_letters]
-        lst_prediction = list(prediction)
-        for index_position in range(len(positions)):
-            if positions[index_position] == "1":
-                lst_prediction[index_position] = colored(prediction[index_position], "green")
-            if positions[index_position] == "2":
-                lst_prediction[index_position] = colored(prediction[index_position], "yellow")
-            if positions[index_position] == "0":
-                lst_prediction[index_position] = colored(prediction[index_position], "red")
-        #  Need to colour incorrect letters
-        if index_position == 4:
-            coloured_guesses.append("".join(lst_prediction))
-            if len(coloured_guesses) == 6:
-                return coloured_guesses
-
+        if positions_letters[index_positions_letters] == "1":
+            lst_prediction[index_positions_letters] = colored(prediction[index_positions_letters], "green")
+        if positions_letters[index_positions_letters] == "2":
+            lst_prediction[index_positions_letters] = colored(prediction[index_positions_letters], "yellow")
+        if positions_letters[index_positions_letters] == "0":
+            lst_prediction[index_positions_letters] = colored(prediction[index_positions_letters], "red")
+        if index_positions_letters == 4:
+            for index_letter in range(len(incorrect_letters)):
+                incorrect_letters[index_letter] = colored(incorrect_letters[index_letter], "red")
+                if index_letter == len(incorrect_letters) - 1:
+                    return "".join(lst_prediction), "".join(incorrect_letters)
             
-
+            
+def gameplay(answer):
+    num_attempts = 1
+    while True:
+        if num_attempts != 7:
+            print("Attempt number " + str(num_attempts) + ":")
+            predictions, wrong_letters = colour_guesses_letters(answer)
+            print(predictions)
+            print(wrong_letters)
+            num_attempts += 1
+            if predictions == answer:
+                prompt_user = input("Winner! Enter Y here to play again, N if not?")
+                if prompt_user == "N":
+                    break
+                else:
+                   num_attempts = 1 
+        else:
+            prompt_user = input("No more available attempts! Enter Y here to play again, N if not? ")
+            if prompt_user == "N":
+                break
+            else:
+                num_attempts = 1
